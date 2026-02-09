e6b111bd9ee1caf0d56167be347d6b44ee8d78f3
Felix Vargas <felix70529@gmail.com>
2026-02-09 10:49:58 -0500

    Wokred on tip

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 // Helper function to fetch all products
 const fetchAllProducts = () => {
-
+  return fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      const products = data.products;
+      return products;
+    });
+  // .catch((error) => {
+  //   console.error(error);
+  // });
 };
+fetchAllProducts();
 
 // Helper function to fetch a single product by ID
-const fetchProductById = (productId) => {
- 
-};
+// const fetchProductById = (productId) => {
+//   fetch("https://dummyjson.com/products/1")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const idProduct = products.find((p) => p.productId);
+//     });
+// };
+// fetchProductById();
 
 // 1. Sum of all prices
-const sumAllPrices = () => {
-  
+const sumAllPrices = (fetchAllProducts) => {
+  fetchAllProducts().then((res) =>
+    res.reduce((product) => {
+      acc += product.price;
+    }, 0),
+  );
 };
-
+sumAllPrices();
 // 2. Get email of all reviewers of product X
-const getReviewerEmails = (productId) => {
- 
-};
+const getReviewerEmails = (productId) => {};
 
 // Export functions for testing
 module.exports = {
   sumAllPrices,
   getReviewerEmails,
   fetchAllProducts,
-  fetchProductById
+  fetchProductById,
 };
